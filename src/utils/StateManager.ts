e702e1831c9e388c03b3bac1810700f73a0916a1
Jerry Wan <jerrywan@Jerrys-MacBook-Pro.local>
2024-06-02 00:42:11 -0700

    refactored serialization/deserialization to single class

--- src/utils/StateManager.ts
+++ src/utils/StateManager.ts
@@ -3,8 +3,21 @@ import path from 'path';
 import Config from './Config.js';
 import StockListManager from './StockListManager.js';
 import TradeExecutor from '../services/TradeExecutor.js';
-export default class Deserializer {
+
+export default class StateManager {
     private static filePath = path.join(__dirname, '../storage/state.json');
+
+    static serialize(tradeExecutor: TradeExecutor): void {
+        const state = {
+            config: Config.toJSON(),
+            stockListManager: StockListManager.toJSON(),
+            tradeExecutor: tradeExecutor.toJSON()
+        };
+
+        const jsonString = JSON.stringify(state, null, 2);
+        fs.writeFileSync(this.filePath, jsonString, 'utf8');
+        console.log('State has been serialized to', this.filePath);
+    }
 
     static deserialize(): void {
         const jsonString = fs.readFileSync(this.filePath).toString();
@@ -14,4 +27,6 @@ export default class Deserializer {
         TradeExecutor.fromJSON(json.tradeExecutor);
         console.log('State has been deserialized from, ', this.filePath);
     }
+
+
 }
